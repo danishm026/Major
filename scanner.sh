7d853629f20006a93dc2309fa31dd610731c78fe
Danish <danishm026@gmail.com>
2016-05-30 12:03:25 +0530

    Commit 3

--- scanner.sh
+++ scanner.sh
@@ -20,72 +20,37 @@ then
 Choose Scan type:
 1. Full TCP Scan
 2. SYN Scan
+3. Ping Scan
+4. Aggressive Scan
+5. Scan without ping
 EOF
 
 	read scan_type
 	
 	#Check if entered scan type is correct
-	if [ $scan_type -gt 2 ] || [ $scan_type -lt 1 ]
+	if [ $scan_type -gt 4 ] || [ $scan_type -lt 1 ]
 	then
 		echo Wrong Scan Type
 		exit 1
 	fi
+	echo 
+	echo Enter IP Address
+	read ip
+	echo Enter port
+	read port
 
-	#Get target type
-	cat <<EOF
-
-Choose Target Type:
-1. Single IP Address Single Port
-2. Single IP Address Multiple Port
-3. Multiple IP Address Single Port
-4. Multiple IP Address Multiple Port
-EOF
-	read target_type
-	
-	if [ $target_type -lt 1 ] || [ $target_type -gt 4 ]
-	then 
-		echo Wrong Target Type
-		exit 2
-	fi
-
-	#If Scan Type is Ful TCP Scan
-	if [ $scan_type = 1 ]
-	then 
-		scan_type=T
-	#Otherwise SYN Scan
-	else
-		scan_type=S
-	fi
-
-	case $target_type in
-		1)
-			echo Enter IP Address
-			read ip
-			echo Enter Port
-			read port
-			echo ""
+	case $scan_type in
+		1) 
+			nmap -sT $ip -p $port
 			;;
 		2)
-			echo Enter Range of IP Address
-			read ip
-			echo Enter Port
-			read port 
-			echo ""
+			nmap -sS $ip -p $port
 			;;
-		3)	
-			echo Enter IP Address
-			read ip
-			echo Enter Port Range
-			read port 
-			echo ""
+		3)
+			nmap -sS -PP -PE -PS $ip
 			;;
 		4)
-			echo Enter Range of IP Address
-			read ip
-			echo Enter Port Range
-			read port 
-			echo ""
+			nmap -sS -A $ip	
 			;;
-	esac
-	nmap -s$scan_type $ip -p $port | tail -n +4 | head -n -2	
+		esac
 fi
